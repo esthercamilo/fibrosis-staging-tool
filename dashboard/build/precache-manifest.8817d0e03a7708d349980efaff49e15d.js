76ca8c4ef003319b8c2081493597caed75a8e3e2
Esther <esther.camilo@gntech.med.br>
2024-12-10 19:46:20 -0300

    corrige static

---
+++ dashboard/build/precache-manifest.8817d0e03a7708d349980efaff49e15d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ade558ecd4c973a6d44",
+    "url": "./fibrosis-staging-tool/static/css/main.dbe0a0eb.chunk.css"
+  },
+  {
+    "revision": "0ade558ecd4c973a6d44",
+    "url": "./fibrosis-staging-tool/static/js/main.0ade558e.chunk.js"
+  },
+  {
+    "revision": "184056ce0fb506e5aaa8",
+    "url": "./fibrosis-staging-tool/static/css/1.f84904a0.chunk.css"
+  },
+  {
+    "revision": "184056ce0fb506e5aaa8",
+    "url": "./fibrosis-staging-tool/static/js/1.184056ce.chunk.js"
+  },
+  {
+    "revision": "17c3314e9dec8e125669",
+    "url": "./fibrosis-staging-tool/static/js/runtime~main.17c3314e.js"
+  },
+  {
+    "revision": "e233f003326cbd1642d14396173f0f14",
+    "url": "./fibrosis-staging-tool/index.html"
+  }
+];
